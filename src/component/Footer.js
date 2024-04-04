295546314b8dff8c6b22537efe24416cbde9ee1d
Dhruv <124017998+Dhruv715@users.noreply.github.com>
2024-04-04 12:01:20 +0530

    Step 4 Changes On Responsiveness

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <div className="bg-black py-10 lg:px-28 sm:px-10 flex flex-col ps-8 lg:flex-row justify-between text-white" style={{ fontFamily: 'poppins' }}>
         <div className="boxsss lg:w-2/5 md:w-full sm:w-full lg:pe-20 mb-10 lg:mb-0">
           <h1 className="pb-3 font-bold text-3xl text-yellow-300 cursor-pointer">MyTaxi.com</h1>
-          <h1 className="pb-3 font-bold text-xl">About Us</h1>
+          <h1 className="pb-3 font-bold text-xl hover:text-yellow-400">About Us</h1>
           <p className="font-bold text-medium text-gray-400">
             Welcome to Style Taxi, your one-stop solution for all your cab
             rental needs in multiple cities across Gujarat, Maharashtra, and
@@ -14,24 +14,24 @@ function Footer() {
           </p>
         </div>
         <div className="lg:w-1/5 md:w-2/5 sm:w-full">
-          <h1 className="pb-6 lg:pb-10 font-bold text-xl cursor-pointer">Style taxi Services</h1>
+          <h1 className="pb-6 lg:pb-10 font-bold text-xl cursor-pointer hover:text-yellow-400">Style taxi Services</h1>
           <ol>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">Routes</li>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">Oneway cab</li>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">Round Trip</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">Routes</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">Oneway cab</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">Round Trip</li>
           </ol>
         </div>
         <div className="lg:w-1/5 md:w-2/5 sm:w-full">
-          <h1 className="pb-6 lg:pb-10 font-bold text-xl cursor-pointer">Support</h1>
+          <h1 className="pb-6 lg:pb-10 font-bold text-xl cursor-pointer hover:text-yellow-400">Support</h1>
           <ol>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">About</li>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">Contact Us</li>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">Privacy Policy</li>
-            <li className="text-gray-400 font-bold pb-3 cursor-pointer">Terms & Conditions</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">About</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">Contact Us</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">Privacy Policy</li>
+            <li className="text-gray-400 font-bold pb-3 cursor-pointer hover:text-yellow-400">Terms & Conditions</li>
           </ol>
         </div>
         <div className="lg:w-1/5 md:w-2/5 sm:w-full">
-          <h1 className="pb-6 lg:pb-10 font-bold text-xl cursor-pointer">Contact</h1>
+          <h1 className="pb-6 lg:pb-10 font-bold text-xl cursor-pointer hover:text-yellow-400">Contact</h1>
           <ol>
             <li className="font-bold pb-3 cursor-pointer"><i class="ri-mail-fill text-yellow-300"></i> &nbsp;MyTaxi76@gmail.com</li>
             <li className="font-bold pb-3 cursor-pointer"><i class="ri-phone-line text-yellow-300"></i> &nbsp;+91 9908977656</li>
--- src/component/Section5.js
+++ src/component/Section5.js
@@ -4,19 +4,19 @@ function Section5() {
   return (
     <>
         <div className='w-screen bg-gray-100 py-10'>
-        <h1 className='font-extrabold text-4xl lg:text-7xl md:text-6xl py-5 text-center' style={{fontFamily:'poppins'}}>Why Choose Style Taxi</h1>
-        <h3 className='font-medium text-center py-3 text-lg'  style={{fontFamily:'poppins'}}>Here are some reasons why Style Taxi is your best choice for renting a cab:</h3>
+        <h1 className='font-extrabold text-center text-4xl lg:text-6xl lg:text-center sm:text-5xl md:text-5xl lg:px-10 lg:py-4 md:py-3 sm:py-4' style={{fontFamily:'poppins'}}>Why Choose Style Taxi</h1>
+        <h3 className='font-medium text-center py-3 text-lg px-4 '  style={{fontFamily:'poppins'}}>Here are some reasons why Style Taxi is your best choice for renting a cab:</h3>
         <div className="parentvox flex flex-col lg:flex-row md:flex-col sm:flex-col my-5 justify-center items-center">
-            <div className="lg:w-1/2 md:w-full sm:w-full flex justify-center lg:justify-end mb-5 lg:mb-0">
+            <div className="lg:w-1/2 md:w-2/2  sm:w-full flex justify-center lg:justify-end mb-5 lg:mb-0">
                 <img src="https://styletaxi.in/wp-content/uploads/2023/06/taxi-sign-car-600x480.jpg" className='w-4/5 lg:w-auto' alt="taxiimages" />
             </div>
             <div className="lg:w-1/2 md:w-full sm:w-full px-2">
-                <ol type="1" className='w-5/5 lg:w-4/5 md:w-full sm:w-full px-5 text-left font-sans font-medium text-base' style={{fontFamily:'poppins'}} >
-                    <li className='py-3'>Extensive Fleet: Our fleet consists of a wide variety of well-maintained and luxurious cars, ensuring that you have the perfect ride for every occasion.</li>
-                    <li className='py-3'>Professional Drivers: Our drivers are experienced, well-trained, and prioritize your safety and comfort. They are knowledgeable about the local routes and strive to provide an excellent customer experience.</li>
-                    <li className='py-3'>Affordable Rates: We offer competitive pricing without compromising on the quality of service. Our transparent pricing policy ensures that you get value for your money.</li>
-                    <li className='py-3'>24/7 Availability: We understand that travel needs can arise at any time. That's why our services are available round the clock, ensuring that you can book a cab whenever you need one.</li>
-                    <li className='py-3'>Easy Booking Process: With our user-friendly online platform and mobile app, booking a cab is quick and hassle-free. Just a few clicks, and your ride will be confirmed.</li>
+                <ol type="1" className='w-5/5 lg:w-5/5 md:w-full sm:w-full px-5 text-left font-sans font-medium text-base' style={{fontFamily:'poppins'}} >
+                    <li className='py-3 text-gray-400'>Extensive Fleet: Our fleet consists of a wide variety of well-maintained and luxurious cars, ensuring that you have the perfect ride for every occasion.</li>
+                    <li className='py-3 text-gray-400'>Professional Drivers: Our drivers are experienced, well-trained, and prioritize your safety and comfort. They are knowledgeable about the local routes and strive to provide an excellent customer experience.</li>
+                    <li className='py-3 text-gray-400'>Affordable Rates: We offer competitive pricing without compromising on the quality of service. Our transparent pricing policy ensures that you get value for your money.</li>
+                    <li className='py-3 text-gray-400'>24/7 Availability: We understand that travel needs can arise at any time. That's why our services are available round the clock, ensuring that you can book a cab whenever you need one.</li>
+                    <li className='py-3 text-gray-400'>Easy Booking Process: With our user-friendly online platform and mobile app, booking a cab is quick and hassle-free. Just a few clicks, and your ride will be confirmed.</li>
                 </ol>
             </div>
         </div>
